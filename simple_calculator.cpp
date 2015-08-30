ee23af65d296548709ea3b7d759757cf264788d2
mfarin <mfarinakhsanta@gmail.com>
2015-08-30 19:25:28 +0700

    Update simple_calculator.cpp

---
+++ simple_calculator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+usingnamespace std
+
+int main()
+{
+  cout<< "kalkulator c++";
+  cout<< "masukkan angka:";
+  cout<< "masukkan perhitungan yang diinginkan:";
+  char a;
+  
+  int a;
+  cin>> a;
