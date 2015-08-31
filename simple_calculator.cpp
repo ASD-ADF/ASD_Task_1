5be0defcb409cd290b5f7e323c7108d232cb4e50
ivantn <ivansk8ergt@gmail.com>
2015-08-31 21:58:57 +0700

    Update simple_calculator.cpp

---
+++ simple_calculator.cpp
@@ -0,0 +1,32 @@
+//kalkulator.cc
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a,b, result;
+    char operation;
+
+    {
+    case '+':
+        result = a - b;
+        break;
+
+    case '-':
+        result = a - b;
+        break;
+    case '*':
+        result = a * b;
+        break;
+    case '/':
+        result = a / b;
+        break;
+
+    default:
+        count << "operasi gagal." << end1;
+        return -1;
+    }
+    //jawaban.
+    count << result << end1;
+    return 0;
+}
