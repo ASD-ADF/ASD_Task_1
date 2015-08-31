e1cab0bb33d5a0b554816b0ed8c250f0344a7741
mfarin <mfarinakhsanta@gmail.com>
2015-08-31 18:06:33 +0700

    Update simple_calculator.cpp

--- simple_calculator.cpp
+++ simple_calculator.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
-usingnamespace std
+using namespace std
 
 int main()
 {
+  int a,b;
+  float hsl;
+  char opr;
+  string c;
+  do {
   cout<< "kalkulator c++";
-  cout<< "masukkan angka:";
+  cout<< "masukkan angka 1: \n";
+  cin>>a;
+  cout<< "masukkan angka 2: \n";
+  cin>>b;
   cout<< "masukkan perhitungan yang diinginkan:";
-  char a;
-  
-  int a;
-  cin>> a;
+  cin>>opr;
+  if (opr=='+')
+    hsl=a+b;
+  else if (opr=='-')
+    hsl=a-b;
+  else if (opr=='*')
+    hsl=a*b;
+  else if (opr=='/')
+    hsl=a/b;
+  cout<< "hasil dari perhitungan adalah:"<<hsl<<".\n";
+  cout<< "apakah ada yang ingin dihitung lagi? (ya/tidak)";
+  cin>>c;
+  }
+  while (c!="tidak")
+}
