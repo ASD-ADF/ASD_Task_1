faf830ea5e72d4b45a888ede3ab6879038534220
renesje <ranarenes.rengga@gmail.com>
2015-08-31 17:35:55 +0700

    Task_1
    Pak ini versi yang sudah diperbaiki

--- Rana Renes_simple_calculator.cpp
+++ Rana Renes_simple_calculator.cpp
@@ -2,31 +2,40 @@
 using namespace std;
 int main()
 {
-    char o;
+    char o, b;
     float angka1,angka2;
-    cout << "Masukkan operator yang anda inginkan, kali (*), bagi (/), tambah (+), atau kurang(-)" << endl;
-    cin >> o;
-    cout << "Masukkan angka 1:" << endl;
-    cin >> angka1;
-    cout << "Masukkan angka 2:" << endl;
-    cin >> angka2;
-    cout << "Hasilnya:" << endl;
-    switch (o) {
+    cout << "Apakah anda ingin menghitung?" << endl;
+    cin >> b;
+
+    while (b == 'Y' )
+    {
+        cout << "Masukkan operator yang anda inginkan, kali (*), bagi (/), tambah (+), atau kurang(-)" << endl;
+        cin >> o;
+        cout << "Masukkan angka 1:" << endl;
+        cin >> angka1;
+        cout << "Masukkan angka 2:" << endl;
+        cin >> angka2;
+        cout << "Hasilnya:" << endl;
+    switch (o)
+    {
     case '+':
-        cout << angka1+angka2;
+        cout << angka1+angka2 << endl;
         break;
     case '-':
-        cout << angka1-angka2;
+        cout << angka1-angka2 << endl;
         break;
     case '*':
-        cout << angka1*angka2;
+        cout << angka1*angka2 << endl;
         break;
     case '/':
-        cout << angka1/angka2;
+        cout << angka1/angka2 << endl;
         break;
     default:
-        cout << "Maaf, operasi yang anda masukkan tidak tersedia";
+        cout << "Maaf, operasi yang anda masukkan tidak tersedia" << endl;
         break;
+    }
+    cout << "Hitung lagi?" << endl;
+    cin >> b;
     }
     return 0;
 }
