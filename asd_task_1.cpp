bdd33f198698de19898b044489ce51fb69c92bcd
RegitaAnjani49 <regitaanjani49@gmail.com>
2016-09-07 04:23:46 -1200

    Update asd_task_1.cpp

--- asd_task_1.cpp
+++ asd_task_1.cpp
@@ -1,7 +1,7 @@
 /**
-    NAMA  :
-    NIM   :
-    KELAS :
+    NAMA  :RegitaAnjani
+    NIM   :1301154477
+    KELAS :IF-39-01
 **/
 
 
@@ -12,12 +12,8 @@ using namespace std;
 /**
     - daftarkan seluruh variable global yang dibutuhkan di sini
 **/
-//=================================================
-// YOUR CODE STARTS HERE
-
-
-// YOUR CODE ENDS HERE
-//=================================================
+	int A=0,B=0,C=0,D=0,nuts,nuas,ntubes,prosentase;
+	char in;
 
 
 void main_menu();
@@ -45,13 +41,22 @@ void main_menu(){
     **/
 
     int pilihan;
-    //=================================================
-    // YOUR CODE STARTS HERE
+    cout<<"DAFTAR MENU"<<endl;
+	cout<<"1.Set standar indeks nilai"<<endl;
+	cout<<"2.Set proporsi nilai"<<endl;
+	cout<<"3.Input nilai"<<endl;
+	cout<<"4.Keluar"<<endl;
+	cout<<"Masukkan Pilihan"<<endl;
+	cin>>pilihan;
 
+		switch(pilihan)
+		{
+		 case 1 : set_standar();break;
+		 case 2 : set_proporsi_nilai();break;
+		 case 3 : input_nilai();break;
+		 case 4 : thank_you();break;
+		}
 
-    // YOUR CODE ENDS HERE
-    //=================================================
-    thank_you();
 }
 
 
@@ -61,12 +66,22 @@ void set_proporsi_nilai(){
     - fungsi akan menerima 3 input prosentase dari user untuk UTS, UAS, dan TUBES
     - fungsi akan mengulang menerima input dari user jika total prosentase != 100
     **/
-    //=================================================
-    // YOUR CODE STARTS HERE
+	cout<<"input Prosentase Nilai dalam persen :"<<endl;
+	cout<<"Masukkan prosentase UTS : ";
+	cin>>nuts;
+	cout<<"Masukkan prosentase UAS : ";
+	cin>>nuas;
+	cout<<"Masukkan prosentase TUBES : ";
+	cin>>ntubes;
 
-
-    // YOUR CODE ENDS HERE
-    //=================================================
+	prosentase = nuts+nuas+ntubes;
+	if(prosentase !=100) {
+		cout<<"total input prosentase !=100, silahkan ulangi lagi"<<endl;
+		set_proporsi_nilai();
+	}
+	
+	main_menu();
+    
 }
 
 void set_standar(){
@@ -75,12 +90,28 @@ void set_standar(){
     - fungsi akan menerima input rentang nilai untuk masing-masing index nilai
     - fungsi akan mengulang menerima input dari user jika terdapat nilai yang overlap
     **/
-    //=================================================
-    // YOUR CODE STARTS HERE
+	cout<<"masukkan standar nilai minimum A : ";
+	cin>>A;
+	cout<<"masukkan standar nilai minimum B : ";
+	cin>>B;
+	cout<<"masukkan standar nilai minimum C : ";
+	cin>>C;
+	cout<<"masukkan standar nilai minimum D : ";
+	cin>>D;
 
+	if(B>A) {
+		cout<<"input standar salah, silahkan ulangi lagi"<<endl;
+		set_standar();
+	}else if (C>B){
+		cout<<"input standar salah, silahkan ulangi lagi"<<endl;
+		set_standar();
+	}else if(D>C){
+		cout<<"input standar salah, silahkan ulangi lagi"<<endl;
+		set_standar();
+	}
 
-    // YOUR CODE ENDS HERE
-    //=================================================
+	main_menu();
+    
 }
 
 
@@ -92,12 +123,20 @@ char hitung_index(double uts, double uas, double tubes){
     **/
 
     char index;
-    //=================================================
-    // YOUR CODE STARTS HERE
+	double sum;
 
+	sum=(((uts*nuts)/100)+((uas*nuas)/100)+((tubes*ntubes)/100));
+	if(sum>=A)
+		index ='A';
+	else if (sum>=B && sum<=A)
+		index='B';
+	else if (sum>=C && sum<=B)
+		index='C';
+	else if (sum>=D && sum<=C)
+		index='D';
+	else
+		index='E';
 
-    // YOUR CODE ENDS HERE
-    //=================================================
     return index;
 }
 
@@ -108,12 +147,16 @@ void input_nilai(){
     - fungsi menampilkan index yang didapat berdasarkan input nilai UTS, UAS, dan TUBES
     **/
     double uas, uts, tubes;
-    //=================================================
-    // YOUR CODE STARTS HERE
+	cout<<"masukkan nilai uts : ";
+	cin>>uts;
+	cout<<"masukkan nilai uas : ";
+	cin>>uas;
+	cout<<"masukkan nilai tubes : ";
+	cin>>tubes;
+	cout<<"indeks nilai anda adalah "<<hitung_index(uts,uas,tubes)<<endl;
 
-
-    // YOUR CODE ENDS HERE
-    //=================================================
+	main_menu();
+    
 }
 
 void thank_you(){
@@ -121,10 +164,9 @@ void thank_you(){
     - fungsi menampilkan pesan singkat untuk mengakhiri program
     - tampilkan nim dan nama kalian
     **/
-    //=================================================
-    // YOUR CODE STARTS HERE
-
-
-    // YOUR CODE ENDS HERE
-    //=================================================
+	cout<<endl;
+	cout<<"Terima Kasih telah meggunakan aplikasi kami"<<endl;
+	cout<<"1301154477 - REGITA ANJANI";
+	cout<<endl;
+    
 }
