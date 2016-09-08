78cb353966efda466bd3e9546b45c26a7531731c
Ichsanal <ichsanal28@gmail.com>
2016-09-08 09:58:27 +0700

    Update asd_task_1.cpp

--- asd_task_1.cpp
+++ asd_task_1.cpp
@@ -1,7 +1,7 @@
 /**
-    NAMA  :
-    NIM   :
-    KELAS :
+    NAMA  : Raden Muhammad Ichsan Al Rasyid
+    NIM   : 1301154295
+    KELAS : IF 39-01
 **/
 
 
@@ -14,7 +14,8 @@ using namespace std;
 **/
 //=================================================
 // YOUR CODE STARTS HERE
-
+double proputs, propuas, protubes = 0;
+double grade[4];
 
 // YOUR CODE ENDS HERE
 //=================================================
@@ -47,6 +48,25 @@ void main_menu(){
     int pilihan;
     //=================================================
     // YOUR CODE STARTS HERE
+    while(pilihan !=4)
+    {
+        cout <<"1. set standar index nilai\n2. set proporsi nilai\n3. input nilai\n4. keluar\nPilih :";
+        cin>> pilihan;
+        switch(pilihan)
+        {
+            case 1 :
+                set_standar();
+                break;
+            case 2 :
+                set_proporsi_nilai();
+                break;
+            case 3 :
+                input_nilai();
+                break;
+
+        }
+    }
+
 
 
     // YOUR CODE ENDS HERE
@@ -55,7 +75,7 @@ void main_menu(){
 }
 
 
-void set_proporsi_nilai(){
+void set_proporsi_nilai()
     /**
     - fungsi mengubah prosentase nilai UTS, UAS, dan TUBES
     - fungsi akan menerima 3 input prosentase dari user untuk UTS, UAS, dan TUBES
@@ -63,13 +83,27 @@ void set_proporsi_nilai(){
     **/
     //=================================================
     // YOUR CODE STARTS HERE
+    {
+        double total;
+        do
+        {
+            cout << "Proposi UTS : ";
+            cin >> proputs;
+            cout << "Proporsi UAS :";
+            cin >> propuas;
+            cout << "Proporsi TUBES :";
+            cin >> protubes;
+            total = proputs + propuas + protubes;
+        }
 
+        while(total !=100);
+    }
 
     // YOUR CODE ENDS HERE
     //=================================================
-}
 
-void set_standar(){
+
+void set_standar()
     /**
     - fungsi mengubah standar index A, B, C, D, dan E
     - fungsi akan menerima input rentang nilai untuk masing-masing index nilai
@@ -78,53 +112,117 @@ void set_standar(){
     //=================================================
     // YOUR CODE STARTS HERE
 
+{
+    for (int i = 0; i < 5; i++)
+        grade[i] = 0;
 
-    // YOUR CODE ENDS HERE
-    //=================================================
+    do
+    {
+        cout << "Grade D = ";
+        cin >> grade[1];
+        if (grade[0] < grade[1])
+        {
+            cout << "Grade C = ";
+            cin >> grade[2];
+            if (grade[1] < grade[2])
+            {
+                cout << "Grade B = ";
+                cin >> grade [3];
+                if (grade[2] < grade[3])
+                {
+                    cout << "Grade A = ";
+                    cin >> grade[4];
+                }
+            }
+        }
+        for (int i = 0; i<5; i++)
+        {
+            if (grade[i] > 100)
+                grade[i] = 0;
+        }
+        if (grade[4] == 0) {
+            cout << "Input grade salah silahkan ulangi lagi" << endl;
+        }
+
+    }
+
+    while(grade[4] == 0);
 }
 
 
-char hitung_index(double uts, double uas, double tubes){
+    // YOUR CODE ENDS HERE
+    //=================================================
+
+
+char hitung_index(double uts, double uas, double tubes)
     /**
     - fungsi menghitung total nilai berdasarkan input parameter dan proporsi nilai
     - fungsi menentukan index nilai berdasarkan standar nilai
     - fungsi mengembalikan karakter index nilai
     **/
 
-    char index;
+
     //=================================================
     // YOUR CODE STARTS HERE
+{
+
+    char index;
+    double total;
+
+    total = (uts*(proputs/100)) + (uas*(propuas/100)) + (tubes*(protubes/100));
+    if (total < grade[1]) index = 'E';
+    if ((total >= grade[1]) && (total < grade[2])) index = 'D';
+    if ((total >= grade[2]) && (total < grade[3])) index = 'C';
+    if ((total >= grade[3]) && (total < grade[4])) index = 'B';
+    if (total >= grade[4]) index = 'A';
+
+    return index;
+
+}
 
 
     // YOUR CODE ENDS HERE
     //=================================================
-    return index;
-}
 
 
-void input_nilai(){
+void input_nilai()
     /**
     - fungsi menerima input nilai UTS, UAS, dan TUBES
     - fungsi menampilkan index yang didapat berdasarkan input nilai UTS, UAS, dan TUBES
     **/
-    double uas, uts, tubes;
     //=================================================
     // YOUR CODE STARTS HERE
+{
 
+double uas, uts, tubes;
+char indeks;
+
+cout << " Masukkan nilai uts : ";
+cin >> uts;
+cout <<  " Masukkan nilai uas : ";
+cin >> uas;
+cout <<  " Masukkan nilai tubes :";
+cin >> tubes;
+indeks = hitung_index(uts,uas,tubes);
+cout << " index dari nilai anda adalah " << indeks << endl;
+
+}
 
     // YOUR CODE ENDS HERE
     //=================================================
-}
 
-void thank_you(){
+
+void thank_you()
     /**
     - fungsi menampilkan pesan singkat untuk mengakhiri program
     - tampilkan nim dan nama kalian
     **/
     //=================================================
     // YOUR CODE STARTS HERE
-
+{
+    cout << " Dengan selesainya tugas ini saya mengucapkan terimakasih, semoga tugas saya bisa bermanfaat buat saya maupun orang lain " << endl;
+    cout << " Raden Muhammad Ichsan Al Rasyid , NIM 1301154295";
+}
 
     // YOUR CODE ENDS HERE
     //=================================================
-}
