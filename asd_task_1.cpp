f6b35853a31dc9f4b2eab0871f39bfff46577ce8
joelandrew22 <joel.andrew22088@gmail.com>
2017-01-24 15:22:46 +0700

    Saya sudah selesai Pak :)

--- asd_task_1.cpp
+++ asd_task_1.cpp
@@ -1,7 +1,7 @@
 /**
-    NAMA  : isikan nama anda
-    NIM   :
-    KELAS :
+    NAMA  : Joel Andrew Montana K.G
+    NIM   : 1301164165
+    KELAS : IF40-02
 **/
 
 
@@ -14,6 +14,7 @@ using namespace std;
 **/
 //=================================================
 // YOUR CODE STARTS HERE
+int nilaiA,nilaiB,nilaiC,nilaiD,persenuas,persentubes,persenuts;
 
 
 // YOUR CODE ENDS HERE
@@ -47,7 +48,27 @@ void main_menu(){
     int pilihan;
     //=================================================
     // YOUR CODE STARTS HERE
-
+    cout<< "Menu Utama" << endl;
+    cout<< "1.Set Standar index nilai"<< endl;
+    cout<< "2.Set proporsi nilai"<< endl;
+    cout<< "3.Input nilai"<< endl;
+    cout<< "4.Keluar"<< endl;
+    cout<< "Masukan Pilihan: ";
+    cin>> pilihan;
+    switch (pilihan) {
+        case 1:
+            set_standar();
+            main_menu();
+        case 2:
+            set_proporsi_nilai();
+            main_menu();
+        case 3:
+            input_nilai();
+            main_menu();
+        case 4:
+            thank_you();
+            
+    }
 
     // YOUR CODE ENDS HERE
     //=================================================
@@ -63,6 +84,16 @@ void set_proporsi_nilai(){
     **/
     //=================================================
     // YOUR CODE STARTS HERE
+    do {
+        cout<< "Masukan persentase masing masing nilai(Persentase menunjukan nilai di angka 100%)";
+        cout<< "\nMasukan nilai persentasi UTS ";
+        cin>> persenuts;
+        cout<< "Masukan nilai persentasi UAS ";
+        cin>> persenuas;
+        cout<< "Masukan nilai persentasi TUBES ";
+        cin>> persentubes;
+    } while (persenuts + persenuas + persentubes !=100);
+    
 
 
     // YOUR CODE ENDS HERE
@@ -77,6 +108,14 @@ void set_standar(){
     **/
     //=================================================
     // YOUR CODE STARTS HERE
+    cout<<"Masukan nilai minimum A: ";
+    cin>> nilaiA;
+    cout<<"Masukan nilai minimum B: ";
+    cin>> nilaiB;
+    cout<<"Masukan nilai minimum C: ";
+    cin>> nilaiC;
+    cout<<"Masukan nilai minimum D: ";
+    cin>> nilaiD;
 
 
     // YOUR CODE ENDS HERE
@@ -94,6 +133,20 @@ char hitung_index(double uts, double uas, double tubes){
     char index;
     //=================================================
     // YOUR CODE STARTS HERE
+    uts = uts * persenuts/100;
+    uas = uas * persenuas/100;
+    tubes = tubes* persentubes/100;
+    
+    if (uts + uas + tubes >= nilaiA) {
+        index = 'A';
+    } else if (uts + uas + tubes >= nilaiB){
+        index = 'B';
+        
+    } else if (uts + uas + tubes >= nilaiD){
+        index = 'D';
+    } else {
+        index = 'E';
+    }
 
 
     // YOUR CODE ENDS HERE
@@ -110,6 +163,13 @@ void input_nilai(){
     double uas, uts, tubes;
     //=================================================
     // YOUR CODE STARTS HERE
+    cout<< "Masukan nilai UTS:  ";
+    cin>> uts;
+    cout<< "Masukan nilai UAS: ";
+    cin>> uas;
+    cout<< "Masukan nilai TUBES: ";
+    cin>> tubes;
+    cout<< "\n\nNilai Index : " << hitung_index(uts, uas, tubes)<< endl;
 
 
     // YOUR CODE ENDS HERE
@@ -123,7 +183,8 @@ void thank_you(){
     **/
     //=================================================
     // YOUR CODE STARTS HERE
-
+    cout<< " Selamat Menikamati Susahnya C++ Semangat Sayang !!!!!";
+    exit(0);
 
     // YOUR CODE ENDS HERE
     //=================================================
