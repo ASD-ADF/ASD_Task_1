1b0049ac7c4e3a012e801d8dbab72173966bb92f
indahanf <indahnuzi@gmail.com>
2017-01-24 09:14:56 +0700

    123

--- asd_task_1.cpp
+++ asd_task_1.cpp
@@ -1,7 +1,7 @@
 /**
-    NAMA  :
-    NIM   :
-    KELAS :
+    NAMA  : INDAH AYU NUR FAUZIAH
+    NIM   : 1301164004
+    KELAS : IF-40-02
 **/
 
 
@@ -14,7 +14,10 @@ using namespace std;
 **/
 //=================================================
 // YOUR CODE STARTS HERE
-
+float uts,uas,tubes;
+float puts, puas,ptubes;
+int a,b,c,d, lanjut;
+float total;
 
 // YOUR CODE ENDS HERE
 //=================================================
@@ -23,7 +26,7 @@ using namespace std;
 void main_menu();
 void set_standar();
 void set_proporsi_nilai();
-char hitung_index(double uts, double uas, double tubes);
+void hitung_index(double uts, double uas, double tubes);
 void input_nilai();
 void thank_you();
 
@@ -47,12 +50,41 @@ void main_menu(){
     int pilihan;
     //=================================================
     // YOUR CODE STARTS HERE
+lanjut=1;
+while (lanjut==1)
+    {
 
+        cout<<"Daftar Menu: "<<endl;
+        cout<<"1. Set Standar Index Nilai "<<endl;
+        cout<<"2. Set Proporsi nilai "<<endl;
+        cout<<"3. Input Nilai "<<endl;
+        cout<<"4. Keluar "<<endl;
+        cout<<"Masukkan Pilihan (1/2/3/4): ";
+        cin>>pilihan;
+        if (pilihan==1)
+        {
+            set_standar();
+
+        }
+        else if (pilihan==2)
+        {
+            set_proporsi_nilai();
+        }
+        else if(pilihan==3)
+        {
+            input_nilai();
+        }
+        else if (pilihan==4)
+        {
+            thank_you();
+        }
+    }
+}
 
     // YOUR CODE ENDS HERE
     //=================================================
-    thank_you();
-}
+
+
 
 
 void set_proporsi_nilai(){
@@ -64,12 +96,26 @@ void set_proporsi_nilai(){
     //=================================================
     // YOUR CODE STARTS HERE
 
+    cout<<"Input prosentase dalam persen"<<endl;
+    cout<<"Masukkan prosentase UTS: ";
+    cin>>puts;
+    cout<<"Masukkan prosentase UAS: ";
+    cin>>puas;
+    cout<<"Masukkan prosentase TUBES: ";
+    cin>>ptubes;
+
+    if(puts+puas+ptubes<100)
+    {
+        set_proporsi_nilai();
+    }
+
+    main_menu();
 
     // YOUR CODE ENDS HERE
     //=================================================
 }
-
 void set_standar(){
+
     /**
     - fungsi mengubah standar index A, B, C, D, dan E
     - fungsi akan menerima input rentang nilai untuk masing-masing index nilai
@@ -78,13 +124,21 @@ void set_standar(){
     //=================================================
     // YOUR CODE STARTS HERE
 
+cout<<"Masukkan standar minimum A: ";
+cin>>a;
+cout<<"Masukkan standar minimum B: ";
+cin>>b;
+cout<<"Masukkan standar minimum C: ";
+cin>>c;
+cout<<"Masukkan standar minimum D: ";
+cin>>d;
+
+main_menu();
 
     // YOUR CODE ENDS HERE
     //=================================================
 }
-
-
-char hitung_index(double uts, double uas, double tubes){
+void hitung_index(float uts, float uas, float tubes, float puts, float puas, float ptubes, int a, int b, int c, int d){
     /**
     - fungsi menghitung total nilai berdasarkan input parameter dan proporsi nilai
     - fungsi menentukan index nilai berdasarkan standar nilai
@@ -95,10 +149,34 @@ char hitung_index(double uts, double uas, double tubes){
     //=================================================
     // YOUR CODE STARTS HERE
 
+total=(puts/100*uts)+(puas/100*uas)+(ptubes/100*tubes);
+cout<<total<<endl;
+if (total>=a)
+{
+    cout<<"index : A";
 
+}
+else if (total>=b)
+{
+    cout<<"index : B";
+
+}
+else if (total>=c)
+{
+    cout<<"index : C";
+}
+else if (total>=d)
+{
+    cout<<"index : D";
+}
+else
+{
+    cout<<"index : E";
+}
+cout<<endl;
     // YOUR CODE ENDS HERE
     //=================================================
-    return index;
+   // return index;
 }
 
 
@@ -110,8 +188,17 @@ void input_nilai(){
     double uas, uts, tubes;
     //=================================================
     // YOUR CODE STARTS HERE
+cout<<"Masukkan nilai UTS: ";
+cin>>uts;
+cout<<"Masukkan nilai UAS: ";
+cin>>uas;
+cout<<"Masukkan nilai Tubes: ";
+cin>>tubes;
 
 
+hitung_index(uts,uas,tubes,puts,puas,ptubes,a,b,c,d);
+
+main_menu();
     // YOUR CODE ENDS HERE
     //=================================================
 }
@@ -124,7 +211,8 @@ void thank_you(){
     //=================================================
     // YOUR CODE STARTS HERE
 
-
+cout<<"Nama : Indah Ayu Nur Fauziah"<<endl;
+cout<<"NIM : 1301164004"<<endl;
     // YOUR CODE ENDS HERE
     //=================================================
 }
