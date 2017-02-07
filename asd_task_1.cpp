4b286c477a6b6575cff589b8c0732a7958e64c83
undeedummy1 <undeed.dummy.1@gmail.com>
2017-02-07 10:37:39 +0700

    Update asd_task_1.cpp

--- asd_task_1.cpp
+++ asd_task_1.cpp
@@ -64,7 +64,9 @@ void set_proporsi_nilai(){
     //=================================================
     // YOUR CODE STARTS HERE
 
-
+    iopiopiopiopiopiopiopiopiopiopiopiopoi
+        iopiopiopiopiopiopiopiopiop
+        iopiopoipiop
     // YOUR CODE ENDS HERE
     //=================================================
 }
