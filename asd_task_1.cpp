9acbd8e1eaa2e65eec67d4a5b58dab23461c8190
raassh <anggadwi2470@gmail.com>
2017-02-07 20:08:33 +0700

    selesai
    selesai

--- asd_task_1.cpp
+++ asd_task_1.cpp
@@ -1,7 +1,7 @@
 /**
-    NAMA  : isikan nama anda
-    NIM   :
-    KELAS :
+    NAMA  : rangga dwi alamsyah
+    NIM   : 1301164030
+    KELAS : IF 40-02
 **/
 
 
@@ -14,7 +14,8 @@ using namespace std;
 **/
 //=================================================
 // YOUR CODE STARTS HERE
-
+int a,b,c,d,e,pil,puts,puas,ptub;
+double uas,uts,tubes,total;
 
 // YOUR CODE ENDS HERE
 //=================================================
@@ -47,7 +48,28 @@ void main_menu(){
     int pilihan;
     //=================================================
     // YOUR CODE STARTS HERE
-
+    cout<<"1. Set Standar Index Nilai"<<endl;
+    cout<<"2. Set Proporsi Nilai"<<endl;
+    cout<<"3. Input Nilai"<<endl;
+    cout<<"0 Keluar"<<endl;
+        do {
+        cin>>pil;
+        switch(pil) {
+        case 1:
+            set_standar();
+            break;
+        case 2:
+            set_proporsi_nilai();
+            break;
+        case 3:
+            input_nilai();
+            break;
+        case 0:
+            break;
+        default:
+            break;
+        }
+    } while (pil=0);
 
     // YOUR CODE ENDS HERE
     //=================================================
@@ -63,8 +85,17 @@ void set_proporsi_nilai(){
     **/
     //=================================================
     // YOUR CODE STARTS HERE
-
-
+    cout<<"masukan presentase UTS:"<<endl;
+    cin>>puts;
+    cout<<"masukan presentase UAS:"<<endl;
+    cin>>puas;
+    cout<<"masukan presentase TUBES:"<<endl;
+    cin>>ptub;
+    if (puts+puas+ptub != 100){
+        cout<<"Total input presentase != 100, silahkan ulangi lagi"<<endl;
+        set_proporsi_nilai();}
+    else{
+    main_menu();}
     // YOUR CODE ENDS HERE
     //=================================================
 }
@@ -77,8 +108,22 @@ void set_standar(){
     **/
     //=================================================
     // YOUR CODE STARTS HERE
-
-
+    cout<<"masukan standar minimum A:"<<endl;
+    cin>>a;
+    cout<<"masukan standar minimum B:"<<endl;
+    cin>>b;
+    cout<<"masukan standar minimum C:"<<endl;
+    cin>>c;
+    cout<<"masukan standar minimum D:"<<endl;
+    cin>>d;
+    cout<<"masukan standar minimum E:"<<endl;
+    cin>>e;
+    if ((e>d)||(d>c)||(c>b)||(b>a)){
+        cout<<"input standar, silahkan ulangi kembali"<<endl;
+        set_standar();}
+    else{
+        main_menu();
+    }
     // YOUR CODE ENDS HERE
     //=================================================
 }
@@ -94,8 +139,22 @@ char hitung_index(double uts, double uas, double tubes){
     char index;
     //=================================================
     // YOUR CODE STARTS HERE
-
-
+    total = (uts*puts/100)+(uas*puas/100)+(tubes*ptub/100);
+    if (total>=a){
+        index = 'A';
+        cout<<"indeks nilai: "<<index<<endl;}
+    else if (total>=b){
+        index = 'B';
+        cout<<"indeks nilai: "<<index<<endl;}
+    else if (total>=c){
+        index = 'C';
+        cout<<"indeks nilai: "<<index<<endl;}
+    else if (total>=d){
+        index = 'D';
+        cout<<"indeks nilai: "<<index<<endl;}
+    else if (total>=e){
+        index = 'E';
+        cout<<"indeks nilai: "<<index<<endl;}
     // YOUR CODE ENDS HERE
     //=================================================
     return index;
@@ -110,7 +169,14 @@ void input_nilai(){
     double uas, uts, tubes;
     //=================================================
     // YOUR CODE STARTS HERE
-
+    cout<<"masukan nilai uts: "<<endl;
+    cin>>uts;
+    cout<<"masukan nilai uas: "<<endl;
+    cin>>uas;
+    cout<<"masukan nilai tubes: "<<endl;
+    cin>>tubes;
+    hitung_index(uts,uas,tubes);
+    main_menu();
 
     // YOUR CODE ENDS HERE
     //=================================================
@@ -123,7 +189,7 @@ void thank_you(){
     **/
     //=================================================
     // YOUR CODE STARTS HERE
-
+    cout<<"Terimakasih"<<endl;
 
     // YOUR CODE ENDS HERE
     //=================================================
