2b81e98ad9e3bf567d91969c0f4d8b78b36fe74f
Muhammad Ihsan Amien Ismandiya <Grim.ican@gmail.com>
2018-01-19 10:31:15 +0700

    Exercise 3 Done

--- asd_task_1.cpp
+++ asd_task_1.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 void view_arr(int arr[100]);
 
-void exercise_1() {
+void exercise_1()
+{
     /**
      TODO:  Write an algorithm which will prints all
             such numbers which are divisible by 7
@@ -15,15 +16,18 @@ void exercise_1() {
 
     // ===========================
     // YOUR CODE HERE
-       for(int i = 100; i<201; i++){
-        if(i%7==0 &&i%5!=0){
+    for(int i = 100; i<201; i++)
+    {
+        if(i%7==0 &&i%5!=0)
+        {
             cout<<i<<",";
         }
     }
     // ===========================
 }
 
-void exercise_2(int n, int p) {
+void exercise_2(int n, int p)
+{
     /**
          TODO:  Write a procedure algorithm to
                 raise any number to any power.
@@ -38,7 +42,8 @@ void exercise_2(int n, int p) {
     // ===========================
 }
 
-void exercise_3(int s1, int s2, int s3) {
+void exercise_3(int s1, int s2, int s3)
+{
     /**
          TODO:  Write a procedure algorithm that determines
                 the type of a Triangle
@@ -48,17 +53,18 @@ void exercise_3(int s1, int s2, int s3) {
 
     // ===========================
     // YOUR CODE HERE
-    cout<<"your code here"<<endl;
-
-
-
-
-
+    if(s1==s2 && s2==s3)
+        cout << "Equilateral triangle";
+    else if(s1==s2 || s1==s3 || s2==s3)
+        cout << "Isosceles triangle";
+    else
+        cout << "Scalene triangle";
     // ===========================
 }
 
 
-void exercise_4() {
+void exercise_4()
+{
     /**
          TODO:  Write a procedure algorithm to continuously
                 receive a numerical input from user
@@ -79,7 +85,8 @@ void exercise_4() {
     view_arr(arr);
 }
 
-int exercise_5(int arr[],int f) {
+int exercise_5(int arr[],int f)
+{
     /**
          TODO:  Write a function algorithm to find a number
                 inside an unsorted array of integer,
@@ -99,7 +106,8 @@ int exercise_5(int arr[],int f) {
     return -1;
 }
 
-void exercise_6(int arr[],int x) {
+void exercise_6(int arr[],int x)
+{
     /**
          TODO:  Write a procedure algorithm to find a number
          inside an unsorted array of integer,
@@ -118,10 +126,12 @@ void exercise_6(int arr[],int x) {
 }
 
 
-void view_arr(int arr[100]) {
+void view_arr(int arr[100])
+{
     /** Function Helper */
     int i=0;
-    while(arr[i]!=0) {
+    while(arr[i]!=0)
+    {
         cout<<arr[i]<<", ";
         i++;
     }
