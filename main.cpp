3072246416f772a80949234e66ef80feffec4a00
adesatriowirawan <adesatrio1@yahoo.com>
2018-01-25 15:58:00 +0700

    1301153615
    ade satrio

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 
-string name = "your name here";
-string nim = "your id here";
-string cls = "your class here";
+string name = "ade satrio wirawan";
+string nim = "1301153615";
+string cls = "IF 40 int";
 
 int main() {
     cout << "Hello world!, "<<name
