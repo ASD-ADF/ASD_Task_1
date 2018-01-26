b1adc3a06b2528e7d65ec03f438870b1ba912e56
Muhammad Naimullah <muh.naimullah2359@gmail.com>
2018-01-26 20:47:34 +0700

    1301160449_task1
    1301160449

--- asd_task_1.cpp
+++ asd_task_1.cpp
@@ -14,13 +14,17 @@ void exercise_1() {
 
     // ===========================
     // YOUR CODE HERE
-    cout<<"your code here"<<endl;
+    cout<<"My code was here!"<<endl;
+    for (int i = 100;  i <201; i++){
+            if (i%7 == 0 and i%5!=0){
+                cout<< i <<" , ";}
 
 
 
 
 
     // ===========================
+}
 }
 
 void exercise_2(int n, int p) {
@@ -31,11 +35,12 @@ void exercise_2(int n, int p) {
 
     // ===========================
     // YOUR CODE HERE
-    cout<<"your code here"<<endl;
-
-
-
-
+    cout<<"My code was here!"<<endl;
+    int a;
+    a = 1;
+    for (int i = a; i <= p ; i++) {
+        a=a*n;
+    } cout << a <<" ";
 
     // ===========================
 }
@@ -50,7 +55,14 @@ void exercise_3(int s1, int s2, int s3) {
 
     // ===========================
     // YOUR CODE HERE
-    cout<<"your code here"<<endl;
+    cout<<"My code was here!"<<endl;
+    if((s1==s2)&&(s2==3)&&(s3==s1)){
+        cout<< "equilateral"<<endl;
+    }else if ((s1!=s2)&&(s2!=s3)&&(s3!=s1)){
+        cout<< "scalene" <<endl;
+    }else{
+        cout<< "isosceles"<<endl;
+        }
 
 
 
@@ -70,12 +82,15 @@ void exercise_4() {
     int arr[100];
     // ===========================
     // YOUR CODE HERE
-    cout<<"your code here"<<endl;
-
-
-
-
-
+    cout<<"My code was here!"<<endl;
+    int i=0;
+    int a;
+    while ((i<101)&&(a!=0)){
+        cout<<"input number: "<<endl;
+        cin >> a;
+        arr[i]=a;
+        i++;
+    }
     // ===========================
     cout<<"array result: ";
     view_arr(arr);
@@ -91,7 +106,16 @@ int exercise_5(int arr[],int f) {
 
     // ===========================
     // YOUR CODE HERE
-    cout<<"your code here"<<endl;
+    cout<<"My code was here!"<<endl;
+    int i=0;
+    int found=-1;
+    while((arr[i]!=0)&&(found==-1)){
+        if(arr[i]==f){
+              found=i;
+           }
+           i++;
+    }
+    return found;
 
 
 
@@ -110,14 +134,32 @@ void exercise_6(int arr[],int x) {
 
     // ===========================
     // YOUR CODE HERE
-    cout<<"your code here"<<endl;
+    cout<<"My code was here!"<<endl;
+    int i = 0;
+    int found=-1;
+
+    while((arr[i]!=0)&&(found==-1)){
+        if(arr[i]==x){
+            found = i;
+        }else{
+            i++;
+        }
+    }
+
+    if(found!=-1){
+        for(i=10;i>found;i--){
+            arr[i+1]=arr[i];
+        }
+        arr[found+1]=-1;
+    }
+}
 
 
 
 
 
     // ===========================
-}
+
 
 
 void view_arr(int arr[100]) {
