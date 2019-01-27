86bbcfd33921b7b68f63cb0e23347371cd01678c
Komang Triolascarya <46770885+mangrio00@users.noreply.github.com>
2019-01-27 21:58:19 +0700

    test

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 
 /** WRITE DOWN YOUR INFORMATION HERE */
-string name = ""; // put your name here
-string ID = ""; // put your student id here
-int group_id = 0; // your Group Number here (1-8)
+string name = "Komang Triolascarya"; // put your name here
+string ID = "1301180055"; // put your student id here
+int group_id = 4; // your Group Number here (1-8)
 
 
 /** FUNCTIONS LIST, DO NOT MODIFY THESE */
