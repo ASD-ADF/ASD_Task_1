a6a04ac16be75f802dbf8d240d7d058a50da7ba8
Djafar <46723175+CrepDagger@users.noreply.github.com>
2019-01-28 21:23:39 +0700

    Upload task 1 files

--- README.md
+++ README.md
@@ -1,80 +1,195 @@
-# Task_1 [2017/2018-II]
+# Task_1 [2019]
 Task 1 : Introduction to C++
+## Deadline: 29th January 2019
 
 ## Reading Material
 * [git cheat sheet](https://education.github.com/git-cheat-sheet-education.pdf)
 * [markdown cheat sheet](https://enterprise.github.com/downloads/en/markdown-cheatsheet.pdf)
 
 ## Workflow
-1. FORK and CLONE this repositori ASD_Task_1
-2. open and modify codes in *.cpp files inside project ASD_Task_1
-3. write your code inside the provided space in each functions/procedures (`asd_task_1.cpp`) 
+1. Discuss the solution with your group members
+2. FORK and CLONE this repositori ASD_Task_1
+3. open and modify codes in *.cpp files inside project ASD_Task_1
 4. COMMIT and PUSH your project
-5. create a Pull Request
+5. create a Pull Request, put your student ID as the Pull Message
 
-## Exercise 1
-TODO: 
-* Write an algorithm which will prints all such numbers which are divisible by 7 but are not a multiple of 5, between 100 and 200 (both included)
-* `procedure exercise_1()`
-* Output should be: 112, 119, 126, …, 196
+## Group Tasks
+* Group 1: Exercise A, G, and J
+* Group 2: Exercise B, H, and J
+* Group 3: Exercise C, I, and K
+* Group 4: Exercise D, E, and K
+* Group 5: Exercise A, F, and K
+* Group 6: Exercise B, I, and K
+* Group 7: Exercise D, E, and J
+* Group 8: Exercise C, H, and J
 
-## Exercise 2
-TODO:
-* Write a procedure algorithm to raise any number to any power.
-* `procedure exercise_2(n, p)`<br>
-Example:<br>
- `integer n = 2`<br>
- `integer p = 3`<br>
- `exercise_2(n, p)`<br>
- `Output : 8`<br><br>
- `integer n = 4`<br>
- `integer p = 2`<br>
- `exercise_2(n, p)`<br>
- `Output : 16`<br>
+## Exercises
+<ol type="A">
+<li><b>Insert Sort (***)</b><br>
+	write a procedure to receive a number in parameter and insert it into an array so that the array result is in ascending order<br>
+	Example:
+	
+```
+n=0;
+insert_sort(arr,n,4);
+view_data_1(arr,n);
+// should print [4]
+
+insert_sort(arr,n,2);
+insert_sort(arr,n,6);
+view_data_1(arr,n);
+// should print [2, 4, 6]
+```
+</li>
+<li><b>Insert Last Unique (**)</b><br>
+	write a procedure to receive a number in parameter and insert it into an array so that no duplicate value exist in the array<br>
+	Example:
+	
+```
+n=0;
+insert_last_unique(arr,n,4);
+view_data_1(arr,n);
+// should print [4]
+
+insert_last_unique(arr,n,4);
+insert_last_unique(arr,n,5);
+insert_last_unique(arr,n,2);
+insert_last_unique(arr,n,5);
+view_data_1(arr,n);
+// should print [4, 5, 2]
+```
+
+</li>
+<li><b>Insert First (**)</b><br>
+	write a procedure to receive a number in parameter and insert it into an array as the first element<br>
+	Example:
+	
+```
+n=0;
+insert_first(arr,n,4);
+view_data_1(arr,n);
+// should print [4]
+
+insert_first(arr,n,5);
+insert_first(arr,n,2);
+insert_first(arr,n,3);
+view_data_1(arr,n);
+// should print [3,2,5,4]
+```
 
 
-## Exercise 3
-TODO:
-* Write a procedure algorithm that determines the type of a Triangle (isosceles, equilateral, or scalene) given the size of the three sides
-* `exercise_3(s1, s2, s3)` <br>
-Example: <br>
- `integer s1: 4 `<br>
- `integer s2 : 6 `<br>
- `integer s3 : 4 `<br>
- `exercise_3(s1, s2, s3) `<br>
- `Output : isosceles `<br><br>
- `integer s1: 3 `<br>
- `integer s2 : 3 `<br>
- `integer s3 : 3 `<br>
- `exercise_3(s1, s2, s3) `<br>
- `Output : equilateral `<br>
+</li>
+<li><b>Insert Last (*)</b><br>
+	write a procedure to receive a number in parameter and insert it into an array as the last element<br>
+	Example:
+	
+```
+n=0;
+insert_last(arr,n,4);
+view_data_1(arr,n);
+// should print [4]
 
-## Exercise 4
-TODO:
-* Write a procedure algorithm to continuously receive a numerical input from user and add it into an array of integer until user inputs number 0
-* `procedure exercise_4()`
-* Try input: 3, 6, 2, 8, 0
+insert_last(arr,n,5);
+insert_last(arr,n,2);
+insert_last(arr,n,3);
+view_data_1(arr,n);
+// should print [4,5,2,3]
+```
 
-## Exercise 5
-TODO:
-* Write a function algorithm to find a number inside an unsorted array of integer, and outputs its index; algorithm will outputs -1 if such number is not found
-* function int exercise_5(arr, f)
-* Array input is formatted from previous exercise, 
-* value 0 indicates the end of array <br>
-Example: <br>
- `integer arr = {3,6,8,2,7,8,1,0,0,0,0,0} `<br>
- `integer f = 2 `<br>
- `integer x = exercise_5(arr, f) `<br>
- `Output = 3 `
+</li>
+<li><b> Search and Delete (***)</b><br>
+	write a procedure to receive a number and delete all number found in the array<br>
+	Example:
+	
+```
+arr = {3, 5, 2, 4, 1, 2, 3, 1, 3}
+n=9;
+search_and_delete(arr,n,3);
+view_data_1(arr,n);
+// should print [5, 2, 4, 1, 2, 1]
+```
 
-## Exercise 6
-TODO:
-* Write a procedure algorithm to find a number inside an unsorted array of integer, then insert a negative one (-1) behind such found number
-* Use previous search function
-* Array input is formatted from previous exercise, 
-* value 0 indicates the end of array <br>
-Example: <br>
- `integer arr = {3,6,8,2,7,8,1,0,0,0,0,0} `<br>
- `integer x = 8 `<br>
- `exercise_6(arr, x) `<br>
- `arr result = {3,6,8,-1, 2,7,8,1,0,0,0,0,0}`
+
+</li>
+<li><b> First and Second (*)</b><br>
+	write a function to find the greatest number and second greatest number inside an array. Return the result as string<br>
+	Example:
+	
+```
+arr = {3, 5, 2, 4, 1, 2, 3, 1, 3}
+n=9;
+string s = first_and_second(arr,n);
+cout<< s;
+// should print greatest = 5, second = 4"
+```
+
+
+</li>
+<li><b>Count and Sum (*)</b><br>
+	write a function to count the odd number inside an array and sum the even number. Return the result as string<br>
+	Example:
+	
+```
+arr = {3, 5, 2, 4, 1, 2, 3, 1, 3}
+n=9;
+string s = count_and_sum(arr,n);
+cout<< s;
+// should print "count odd = 6, sum even = 8"
+```
+
+
+</li>
+<li><b>Group and Average Data (**)</b><br>
+	write a function to group the numbers in array into odd-even group in a string that also write its average<br>
+	Example:
+	
+```
+arr = {3, 5, 2, 4, 1, 2, 3, 1, 3}
+n=9;
+string s = group_data(arr,n);
+cout<< s;
+// should print "3 5 1 3 1 3 2 4 2, average = 2.67"
+```
+
+
+</li>
+<li><b>Swap Data (**)</b><br>
+	write a procedure to swap all number inside an array front to end (1-2-3 into 3-2-1)<br>
+	Example:
+	
+```
+arr = {3, 5, 2, 4, 1, 2, 3, 1, 3}
+n=9;
+swap_data(arr,n);
+view_data_1(arr,n);
+// should print [3, 1, 3, 2, 1, 4, 2, 5, 3]
+```
+
+
+</li>
+<li><b> View Data 1 (*)</b><br>
+	write a procedure to view all number inside an array (front to end)<br>
+	Example:
+	
+```
+arr = {3, 5, 2, 4, 1, 2, 3, 1, 3}
+n=9;
+view_data_1(arr,n);
+// should print [3, 5, 2, 4, 1, 2, 3, 1, 3]
+```
+
+
+</li>
+<li><b>View Data 2 (*)</b><br>
+	write a procedure to view all number inside an array, but in backward (from behind to front)<br>
+	Example:
+	
+```
+arr = {3, 5, 2, 4, 1, 2, 3, 1, 3}
+n=9;
+view_data_1(arr,n);
+// should print [3, 1, 3, 2, 1, 4, 2, 5, 3]
+```
+	
+</li>
+</ol>
--- main.cpp
+++ main.cpp
@@ -1,130 +1,603 @@
 #include <iostream>
-#include "asd_task_1.h"
+#include <iomanip>
 
 using namespace std;
 
-string name = "your name here";
-string nim = "your id here";
-string cls = "your class here";
+
+/** WRITE DOWN YOUR INFORMATION HERE */
+string name = "Djafar Shiddieq Ritonga"; // put your name here
+string ID = "1301184367"; // put your student id here
+int group_id = 2; // your Group Number here (1-8)
+
+
+/** FUNCTIONS LIST, DO NOT MODIFY THESE */
+void insert_sort(int arr[], int &n, int x);
+void insert_last_unique(int arr[], int &n, int x);
+void insert_first(int arr[], int &n, int x);
+void insert_last(int arr[], int &n, int x);
+void search_and_delete(int arr[], int &n, int x);
+string first_and_second(int arr[], int n);
+string count_and_sum(int arr[], int n);
+string group_and_average(int arr[], int n);
+void swap_data(int arr[], int n);
+void view_data_1(int arr[], int n);
+void view_data_2(int arr[], int n);
+void check_group(int id);
+
 
 int main() {
-    cout << "Hello world!, "<<name
-         <<",\nid: " << nim
-         <<"\nfrom " << cls<< endl;
+    cout<<"===================== TASK 1 ====================="<<endl;
+    cout<<"Hello, "<<name<<", "<<ID<<endl;
+    cout<<"let's check your exercises"<<endl<<endl;
 
-    cout<<endl<<"------------------------------"<<endl;
-    cout<<"Exercise 1"<<endl;
-    cout<<"Output should be:"<<endl;
-    cout<<"112, 119, 126, 133, 147, 154, 161, 168, 182, 189, 196,"<<endl;
-    cout<<"Your Answer:  "<<endl;
-    exercise_1();
-    cout<<endl<<endl<<"press Enter to continue"<<endl;
-    cin.get();
+    if( group_id>0 && group_id<9) {
+        check_group(group_id);
 
-    cout<<"------------------------------"<<endl;
-    cout<<"Exercise 2"<<endl;
-    int n=2;
-    int p=3;
-    cout<<"input= 2, 3"<<endl;
-    cout<<"Output should be: 8"<<endl;
-    cout<<"Your Answer     : ";
-    exercise_2(n, p);
-    n=4;
-    p=2;
-    cout<<endl;
-    cout<<"input= 4, 2"<<endl;
-    cout<<"Output should be: 16"<<endl;
-    cout<<"Your Answer     : ";
-    exercise_2(n, p);
-    cout<<endl<<endl<<"press Enter to continue"<<endl;
-    cin.get();
-
-    cout<<"------------------------------"<<endl;
-    cout<<"Exercise 3"<<endl;
-    int s1=4;
-    int s2=6;
-    int s3=4;
-    cout<<"input= 4, 6, 4"<<endl;
-    cout<<"Output should be: isosceles"<<endl;
-    cout<<"Your Answer     : ";
-    exercise_3(s1, s2, s3);
-    s1=3;
-    s2=3;
-    s3=3;
-    cout<<endl;
-    cout<<"input= 3, 3, 3"<<endl;
-    cout<<"Output should be: equilateral"<<endl;
-    cout<<"Your Answer     : ";
-    exercise_3(s1, s2, s3);
-    s1=1;
-    s2=2;
-    s3=3;
-    cout<<endl;
-    cout<<"input= 1, 2, 3"<<endl;
-    cout<<"Output should be: scalene"<<endl;
-    cout<<"Your Answer     : ";
-    exercise_3(s1, s2, s3);
-    s1=3;
-    s2=4;
-    s3=4;
-    cout<<endl;
-    cout<<"input= 3, 4, 4"<<endl;
-    cout<<"Output should be: isosceles"<<endl;
-    cout<<"Your Answer     : ";
-    exercise_3(s1, s2, s3);
-    cout<<endl<<endl<<"press Enter to continue"<<endl;
-    cin.get();
-
-    cout<<"------------------------------"<<endl;
-    cout<<"Exercise 4"<<endl;
-    cout<<"try inputs: 3, 6, 2, 8, 0"<<endl;
-    cout<<"Your Answer:  "<<endl;
-    exercise_4();
-    cout<<endl<<endl<<"press Enter to continue"<<endl;
-    cin.get();
-    cin.get();
-
-    cout<<"------------------------------"<<endl;
-    cout<<"Exercise 5"<<endl;
-    int arr[] = {3,6,8,2,7,8,1,0,0,0,0,0};
-    cout<<"Initial Array   : ";
-    view_arr(arr);
-    cout<<endl;
-    int f = 7;
-    int x;
-    cout<<"input= 7"<<endl;
-    cout<<"Output should be: 4"<<endl;
-    x = exercise_5(arr, f);
-    cout<<"Your Answer     : "<<x<<endl;
-    f = 8;
-    cout<<"input= 8"<<endl;
-    cout<<"Output should be: 2"<<endl;
-    x = exercise_5(arr, f);
-    cout<<"Your Answer     : "<<x<<endl;
-    f = 9;
-    cout<<"input= 9"<<endl;
-    cout<<"Output should be: -1"<<endl;
-    x = exercise_5(arr, f);
-    cout<<"Your Answer     : "<<x<<endl;
-    cout<<endl<<endl<<"press Enter to continue"<<endl;
-    cin.get();
-
-    cout<<"------------------------------"<<endl;
-    cout<<"Exercise 6"<<endl;
-    int arr2[] = {3,6,8,2,7,8,1,0,0,0,0,0};
-    cout<<"Initial Array   : ";
-    view_arr(arr);
-    cout<<endl;
-    x = 8;
-    cout<<"input= 8"<<endl;
-    cout<<"Output should be: 3, 6, 8, -1, 2, 7, 8, 1"<<endl;
-    cout<<"Your Answer     : ";
-    exercise_6(arr2, x);
-    view_arr(arr2);
-    cout<<endl<<endl<<"press Enter to continue"<<endl;
-    cin.get();
-
-    cout<<endl<<endl<<"End of Task I"<<endl;
-    cout<<"Congratulation on completing"<<endl;
+        cout<<endl<<endl<<"End of Task I"<<endl;
+        cout<<"Congratulation on completing"<<endl;
+    } else {
+        cout<<"ERROR\nPlease check your personal information above!!"<<endl<<endl;
+    }
     return 0;
+}
+
+
+
+/**=================================================*/
+/**             START YOUR WORK HERE                */
+
+void insert_sort(int arr[], int &n, int x) {
+    /**
+    TODO: write a procedure to receive a number in parameter and insert it into an array so that the array result is in ascending order
+    arr : input array
+    n   : number of element inside array, n should increment by 1 after this procedure executed
+    x   : number to be inserted
+    */
+
+    // YOUR CODES HERE
+    //-----------------------
+
+
+    //-----------------------
+}
+
+
+void insert_last_unique(int arr[], int &n, int x) {
+    /**
+    TODO: write a procedure to receive a number in parameter and insert it into an array so that no duplicate value exist in the array
+    arr : input array
+    n   : number of element inside array, n should increment by 1 after this procedure executed
+    x   : number to be inserted
+    */
+
+    // YOUR CODES HERE
+    //-----------------------
+    int i;
+    i = n-1;
+    while (i > -1 && x != arr[i]) {
+        i--;
+    }
+
+    if (i < 0) {
+        arr[n] = x;
+        n++;
+    } else {
+        arr[n] = 0;
+    }
+    //-----------------------
+}
+
+
+void insert_first(int arr[], int &n, int x) {
+    /**
+    TODO: write a procedure to receive a number in parameter and insert it into an array as the first element
+    arr : input array
+    n   : number of element inside array, n should increment by 1 after this procedure executed
+    x   : number to be inserted
+    */
+
+    // YOUR CODES HERE
+    //-----------------------
+
+
+    //-----------------------
+}
+
+
+void insert_last(int arr[], int &n, int x) {
+    /**
+    TODO: write a procedure to receive a number in parameter and insert it into an array as the last element
+    arr : input array
+    n   : number of element inside array, n should increment by 1 after this procedure executed
+    x   : number to be inserted
+    */
+
+    // YOUR CODES HERE
+    //-----------------------
+
+
+    //-----------------------
+}
+
+
+void search_and_delete(int arr[], int &n, int x) {
+    /**
+    TODO: write a procedure to receive a number and delete all number found in the array
+    arr : input array
+    n   : number of element inside array, n should decreased by the number of deleted elements from the array
+    x   : number to be inserted
+    */
+
+    // YOUR CODES HERE
+    //-----------------------
+
+
+    //-----------------------
+}
+
+
+string first_and_second(int arr[], int n) {
+    /**
+    TODO: write a function to find the greatest number and second greatest number inside an array. Return the result as string
+    arr : input array
+    n   : number of element inside array
+    */
+
+    // YOUR CODES HERE
+    //-----------------------
+
+
+    //-----------------------
+    return "";
+}
+
+
+string count_and_sum(int arr[], int n) {
+    /**
+    TODO: write a function to count the odd number inside an array and sum the even number. Return the result as string
+    arr : input array
+    n   : number of element inside array
+    */
+
+    // YOUR CODES HERE
+    //-----------------------
+
+
+    //-----------------------
+    return "";
+}
+
+
+string group_and_average(int arr[], int n) {
+    /**
+    TODO: write a function to group the numbers in array into odd-even group in a string that also write its average
+    arr : input array
+    n   : number of element inside array
+    */
+
+    // YOUR CODES HERE
+    //-----------------------
+    float jumlah = 0;
+    float banyak = 0;
+    float rata = 0;
+    for (n=0;n<20;n++) {
+        if(arr[n]%2!= 0 && arr[n]<20 && arr[n]>-1) {
+            jumlah += arr[n];
+            banyak += 1;
+            cout<< arr[n]<<" ";
+        }
+    }
+    for(n=0; n<20; n++) {
+        if (arr[n]%2 == 0 && arr[n]<20 && arr[n]>0) {
+            jumlah += arr[n];
+            banyak += 1;
+            cout<< arr[n]<<" ";
+        }
+    }
+
+    rata = (jumlah/=banyak);
+    cout<<", average = "<<setprecision(3)<<rata<<endl;
+    //-----------------------
+    return "";
+}
+
+
+void swap_data(int arr[], int n) {
+    /**
+    TODO: write a procedure to swap all number inside an array front to end (1-2-3 into 3-2-1)
+    arr : input array
+    n   : number of element inside array
+    */
+
+    // YOUR CODES HERE
+    //-----------------------
+
+
+    //-----------------------
+}
+
+
+void view_data_1(int arr[], int n) {
+    /**
+    TODO: write a procedure to view all number inside an array (front to end)
+    arr : input array
+    n   : number of element inside array
+    */
+
+    // YOUR CODES HERE
+    //-----------------------
+    for(int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    //-----------------------
+}
+
+
+void view_data_2(int arr[], int n) {
+    /**
+    TODO: write a procedure to view all number inside an array, but in backward (from behind to front)
+    arr : input array
+    n   : number of element inside array
+    */
+
+    // YOUR CODES HERE
+    //-----------------------
+
+
+    //-----------------------
+}
+
+
+void check_group(int id) {
+    cout<<"Checking Work for Group "<<id<<endl;
+
+    switch(id) {
+    case 1: {
+        int n=0;
+        int arr[20];
+        insert_sort(arr,n,4);
+        cout<<"expected output: 4"<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+        // should print [4]
+
+        cout<<endl;
+        insert_sort(arr,n,2);
+        cout<<"expected output: 2, 4"<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl;
+        insert_sort(arr,n,6);
+        cout<<"expected output: 2, 4, 6"<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl;
+        insert_sort(arr,n,5);
+        cout<<"expected output: 2, 4, 5, 6"<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl<<endl<<"press Enter to continue"<<endl;
+        cin.get();
+
+        view_data_1(arr,n);
+        string s = count_and_sum(arr,n);
+        cout<<"expected output: count odd = 1, sum even = 12"<<endl;
+        cout<<"your output    : "<< s;
+
+        break;
+    }
+    case 2: {
+        int n=0;
+        int arr[20];
+        insert_last_unique(arr,n,4);
+        cout<<"expected output: 4"<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl;
+        insert_last_unique(arr,n,4);
+        cout<<"expected output: 4"<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl;
+        insert_last_unique(arr,n,5);
+        cout<<"expected output: 4, 5"<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl;
+        insert_last_unique(arr,n,2);
+        cout<<"expected output: 4, 5, 2"<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl;
+        insert_last_unique(arr,n,5);
+        cout<<"expected output: 4, 5, 2"<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl<<endl<<"press Enter to continue"<<endl;
+        cin.get();
+
+        view_data_1(arr,n);
+        string s = group_and_average(arr,n);
+        cout<<"expected output: 5 4 2, average = 3.67"<<endl;
+        cout<<"your output    : "<< s;
+
+        break;
+    }
+    case 3: {
+        int n=0;
+        int arr[20];
+        insert_first(arr,n,4);
+        cout<<"expected output: 4"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl;
+        insert_first(arr,n,5);
+        cout<<"expected output: 4, 5 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl;
+        insert_first(arr,n,2);
+        cout<<"expected output: 4, 5, 2 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl;
+        insert_first(arr,n,3);
+        cout<<"expected output: 4, 5, 2, 3 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl;
+        insert_first(arr,n,5);
+        cout<<"expected output: 4, 5, 2, 3, 5 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl<<endl<<"press Enter to continue"<<endl;
+        cin.get();
+
+        view_data_2(arr,n);
+        swap_data(arr,n);
+        view_data_1(arr,n);
+        cout<<"expected output: 5, 3, 2, 5, 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        break;
+    }
+    case 4: {
+        int n=0;
+        int arr[20];
+        insert_last(arr,n,4);
+        cout<<"expected output: 4"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl;
+        insert_last(arr,n,5);
+        cout<<"expected output: 5, 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl;
+        insert_last(arr,n,2);
+        cout<<"expected output: 2, 5, 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl;
+        insert_last(arr,n,3);
+        cout<<"expected output: 3, 2, 5, 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl;
+        insert_last(arr,n,5);
+        cout<<"expected output: 5, 3, 2, 5, 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl<<endl<<"press Enter to continue"<<endl;
+        cin.get();
+
+        view_data_2(arr,n);
+        search_and_delete(arr,n, 5);
+        view_data_2(arr,n);
+        cout<<"expected output: 3, 2, 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        break;
+    }
+    case 5: {
+        int n=0;
+        int arr[20];
+        insert_sort(arr,n,4);
+        cout<<"expected output: 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl;
+        insert_sort(arr,n,2);
+        cout<<"expected output: 4, 2 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl;
+        insert_sort(arr,n,6);
+        cout<<"expected output: 6, 4, 2 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl;
+        insert_sort(arr,n,5);
+        cout<<"expected output: 6, 5, 4, 2 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl;
+        insert_sort(arr,n,6);
+        cout<<"expected output: 6, 6, 5, 4, 2 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl<<endl<<"press Enter to continue"<<endl;
+        cin.get();
+
+        view_data_2(arr,n);
+        string s = first_and_second(arr,n);
+        cout<<"expected output: greatest = 6, second = 5"<<endl;
+        cout<<"your output    : "<< s;
+
+        break;
+    }
+    case 6: {
+        int n=0;
+        int arr[20];
+        insert_last_unique(arr,n,4);
+        cout<<"expected output: 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl;
+        insert_last_unique(arr,n,4);
+        cout<<"expected output: 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl;
+        insert_last_unique(arr,n,5);
+        cout<<"expected output: 5, 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl;
+        insert_last_unique(arr,n,2);
+        cout<<"expected output: 2, 5, 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl;
+        insert_last_unique(arr,n,5);
+        cout<<"expected output: 2, 5, 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl;
+        insert_last_unique(arr,n,8);
+        cout<<"expected output: 8, 2, 5, 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        cout<<endl<<endl<<"press Enter to continue"<<endl;
+        cin.get();
+
+        view_data_2(arr,n);
+        swap_data(arr,n);
+        view_data_1(arr,n);
+        cout<<"expected output: 4, 5, 2, 8 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_2(arr,n);
+
+        break;
+    }
+    case 7: {
+        int n=0;
+        int arr[20];
+        insert_last(arr,n,4);
+        cout<<"expected output: 4"<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl;
+        insert_last(arr,n,5);
+        cout<<"expected output: 5, 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl;
+        insert_last(arr,n,2);
+        cout<<"expected output: 2, 5, 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl;
+        insert_last(arr,n,3);
+        cout<<"expected output: 3, 2, 5, 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl;
+        insert_last(arr,n,5);
+        cout<<"expected output: 5, 3, 2, 5, 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl<<endl<<"press Enter to continue"<<endl;
+        cin.get();
+
+        view_data_1(arr,n);
+        search_and_delete(arr,n, 5);
+        view_data_1(arr,n);
+        cout<<"expected output: 3, 2, 4 (reversed)"<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        break;
+    }
+    case 8: {
+        int n=0;
+        int arr[20];
+        insert_first(arr,n,4);
+        cout<<"expected output: 4"<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl;
+        insert_first(arr,n,5);
+        cout<<"expected output: 5, 4"<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl;
+        insert_first(arr,n,2);
+        cout<<"expected output: 2, 5, 4 "<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl;
+        insert_first(arr,n,3);
+        cout<<"expected output: 3, 2, 5, 4, "<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl;
+        insert_first(arr,n,5);
+        cout<<"expected output: 5, 3, 2, 5, 4, "<<endl;
+        cout<<"your output    : ";
+        view_data_1(arr,n);
+
+        cout<<endl<<endl<<"press Enter to continue"<<endl;
+        cin.get();
+
+        view_data_1(arr,n);
+        string s = group_and_average(arr,n);
+        cout<<"expected output: 5 3 5 2 4, average = 3.8"<<endl;
+        cout<<"your output    : "<< s;
+
+        break;
+    }
+    }
+    cout<<endl;
 }
