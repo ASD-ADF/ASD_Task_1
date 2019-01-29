8309c450f255cf92c1f1be6bd55c9aa4d477f019
adhityahardy <44922464+adhityahardy@users.noreply.github.com>
2019-01-29 07:54:35 +0700

    1301184036

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 
 /** WRITE DOWN YOUR INFORMATION HERE */
-string name = ""; // put your name here
-string ID = ""; // put your student id here
-int group_id = 0; // your Group Number here (1-8)
+string name = "Adhitya Aldira Hardy"; // put your name here
+string ID = "1301184036"; // put your student id here
+int group_id = 8; // your Group Number here (1-8)
 
 
 /** FUNCTIONS LIST, DO NOT MODIFY THESE */
@@ -87,7 +87,6 @@ void insert_first(int arr[], int &n, int x) {
 
     // YOUR CODES HERE
     //-----------------------
-
 
     //-----------------------
 }
