696ff01a2d6767f518969025f60e7c938e286cdf
kashishkosta <73001514+kashishkosta@users.noreply.github.com>
2020-10-18 01:36:45 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b,c;
+//     cout<<"Enter first number: ";
+//     cin>>a;
+//     cout<<" Enter second number: ";
+//     cin>>b;
+//     c=a+b;
+//     cout<<"The sum is"<<c<<endl;
 #include <iostream>
 
 using namespace std;
